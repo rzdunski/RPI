bafa3da4be87cad401f4a020470c6f45dfa4fd93
Radek <radoslaw.zdunski@gmail.com>
2016-09-30 09:24:37 +0200

    added  function descripton

--- watchodog_final.py
+++ watchodog_final.py
@@ -7,19 +7,13 @@ import subprocess
 url = 'http://192.168.1.18:5000'
 
 def watchdog(url):
+    """function verifing proper work of script named as remote_control.py"""
     cmd = os.system('wget -t 1 -T 5 --delete-after ' + url)
-    #print "cmd result: %d" % cmd
-    #print "url:" , url
     if cmd != 0:
         pidlist = subprocess.check_output(['pidof','python','remote_control.py'])
-	#print "pidlist:" , pidlist.rsplit()
-	#print "os.getpid():" , os.getpid()
-        for pid in range(len(pidlist.rsplit())):
-            #print "pidlist.rsplit()[pid]:%s" % pidlist.rsplit()[pid]
-	    #print "pid:%s" % str(pid)
+	for pid in range(len(pidlist.rsplit())):
             if int(pidlist.rsplit()[pid]) == os.getpid():
-	        #print "os.getpid()%s:" % os.getpid()
-		continue
+	        continue
 	    else:
 	        subprocess.call(['sudo','kill',pidlist.rsplit()[pid]])
 	        time.sleep(2)
